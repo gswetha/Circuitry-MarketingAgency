d27ea84ef7d3475b96d43b46f366cf3521e489eb
Swetha <gswetha@hotmail.com>
2013-11-14 08:50:58 -0800

    Front-end Changes - Circuitry
    - Default screen is page 1
    - Default display is none for menu options
    - Increase number of options
    - Remove unwanted code

--- app/application/views/_header_top_bar.php
+++ app/application/views/_header_top_bar.php
@@ -1,17 +1,13 @@
 	<!-- Top omnipresent bar for global navigation -->
-<!-- 	<div class="topbar">
-		<div class="fill">
-			<div class="menu" id="logo"><img src="/assets/img/company_logo.png" alt="Circuitry"/></div>
-			<div class="menu" id="menu"><img src="/assets/img/menu_icon.png" alt="Circuitry"/></div>
-		</div>
-	</div> -->
-
 <span id="menu_nav">
 	<img src="/assets/img/menu_icon.png" id="menu" alt="Circuitry"/>
 		<ul id="menu_options">
-			<li><hr>Option 1<hr></li>
-			<li>Option 2<hr></li>
-			<li>Option 3<hr></li>
+			<li><hr>Page 1<hr></li>
+			<li>Page 2<hr></li>
+			<li>Page 3<hr></li>
+			<li>Page 4<hr></li>
+			<li>Page 5<hr></li>
+			<li>Page 6<hr></li>
 		</ul>
 	<img src="/assets/img/company_logo.png" id="logo" alt="Circuitry"/>
 </span>
--- app/application/views/home.php
+++ app/application/views/home.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Circuitry | Marketing Agency</title>
+	<title>Circuitry | Crafting Quality Brands</title>
 </head>
 <?php include_once($this->config->item('base_path') .'application/views/_header_styles.php'); ?>
 <body style="padding:0; margin:0; position: relative; width:100%; height:100%;">
@@ -11,8 +11,8 @@
 		<div id="myCarousel" class="carousel slide" data-ride="carousel" style="width:100%; height:100%;">
   		<!-- Indicators -->
 		  <ol class="carousel-indicators">
-		    <li data-target="#myCarousel" data-slide-to="0"></li>
-		    <li data-target="#myCarousel" data-slide-to="1" class="active"></li>
+		    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+		    <li data-target="#myCarousel" data-slide-to="1"></li>
 		    <li data-target="#myCarousel" data-slide-to="2"></li>
 		    <li data-target="#myCarousel" data-slide-to="3"></li>
 		    <li data-target="#myCarousel" data-slide-to="4"></li>
@@ -20,10 +20,10 @@
 		  </ol>
 		  <?php include_once($this->config->item('base_path') . 'application/views/_header_top_bar.php'); ?>
 	        <div class="carousel-inner" style="width:100%; height:100%;">
-	            <div id='page1' class="item">
+	            <div id='page1' class="item active">
 	            	<p>hello page 1</p>
 	            </div>
-	            <div id='page2' class="item active">
+	            <div id='page2' class="item">
 	            	<p>hello page 2</p>
 	            </div>
 	            <div id='page3' class="item">
@@ -47,17 +47,11 @@
 <?php include_once($this->config->item('base_path') . 'application/views/_footer_script.php'); ?>
 </html>
 <script>
-	$('#menu_nav ul')
-	.css({cursor: "pointer"})
-	.on('click', function(){
-	  $(this).find('ul').toggle();
+	$('#menu_nav').on('click', function(){
+	  if ($(this).find("ul").is(':visible')) {
+	  	$(this).find("ul").slideUp();
+	  } else {
+	  	$(this).find("ul").slideDown();
+	  }
 	})
-// $(document).ready(function() {
-//   $('.carousel').carousel();
-//   $('.carousel').on('slid', function() {
-//     var to_slide = $('.carousel-inner .item.active').attr('id');
-//     $('.carousel-indicators').children().removeClass('active');
-//     $('.carousel-indicators [data-slide-to=' + to_slide + ']').addClass('active');
-//   });
-// });
-// </script>
+</script>
