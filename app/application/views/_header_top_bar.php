17a5091d46dfd60e2b240fd181c642e99c7d800a
Swetha <gswetha@hotmail.com>
2013-11-14 10:17:48 -0800

    Menu options to link to different pages

--- app/application/views/_header_top_bar.php
+++ app/application/views/_header_top_bar.php
@@ -2,12 +2,12 @@
 <span id="menu_nav">
 	<img src="/assets/img/menu_icon.png" id="menu" alt="Circuitry"/>
 		<ul id="menu_options">
-			<li><hr>Page 1<hr></li>
-			<li>Page 2<hr></li>
-			<li>Page 3<hr></li>
-			<li>Page 4<hr></li>
-			<li>Page 5<hr></li>
-			<li>Page 6<hr></li>
+			<li data-target="#myCarousel" data-slide-to="0"><hr>Page 1<hr></li>
+			<li data-target="#myCarousel" data-slide-to="1">Page 2<hr></li>
+			<li data-target="#myCarousel" data-slide-to="2">Page 3<hr></li>
+			<li data-target="#myCarousel" data-slide-to="3">Page 4<hr></li>
+			<li data-target="#myCarousel" data-slide-to="4">Page 5<hr></li>
+			<li data-target="#myCarousel" data-slide-to="5">Page 6<hr></li>
 		</ul>
 	<img src="/assets/img/company_logo.png" id="logo" alt="Circuitry"/>
 </span>
