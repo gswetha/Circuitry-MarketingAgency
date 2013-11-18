c66fc5fde575603266cea4370d52b7c1abfe41de
Swetha <gswetha@hotmail.com>
2013-11-18 15:53:35 -0800

    CSS and Text Changes

--- app/application/views/_header_top_bar.php
+++ app/application/views/_header_top_bar.php
@@ -2,12 +2,12 @@
 <span id="menu_nav">
 	<img src="/assets/img/menu_icon.png" id="menu" alt="Circuitry"/>
 		<ul id="menu_options">
-			<li data-target="#myCarousel" data-slide-to="0"><hr>Page 1<hr></li>
-			<li data-target="#myCarousel" data-slide-to="1">Page 2<hr></li>
-			<li data-target="#myCarousel" data-slide-to="2">Page 3<hr></li>
-			<li data-target="#myCarousel" data-slide-to="3">Page 4<hr></li>
-			<li data-target="#myCarousel" data-slide-to="4">Page 5<hr></li>
-			<li data-target="#myCarousel" data-slide-to="5">Page 6<hr></li>
+			<li data-target="#myCarousel" data-slide-to="0"><hr><p class="customfont" style="colour:white; padding=0;">HOME</p><hr></li>
+			<li data-target="#myCarousel" data-slide-to="1"><p class="customfont" style="colour:white; padding=0;">ABOUT US</p><hr></li>
+			<li data-target="#myCarousel" data-slide-to="2"><p class="customfont" style="colour:white; padding=0;">SERVICES</p><hr></li>
+			<li data-target="#myCarousel" data-slide-to="3"><p class="customfont" style="colour:white; padding=0;">PORTFOLIO</p><hr></li>
+			<li data-target="#myCarousel" data-slide-to="4"><p class="customfont" style="colour:white; padding=0;">METHOD</p><hr></li>
+			<li data-target="#myCarousel" data-slide-to="5"><p class="customfont" style="colour:white; padding=0;">CONTACT</p><hr></li>
 		</ul>
 	<img src="/assets/img/company_logo.png" id="logo" alt="Circuitry"/>
 </span>
--- app/application/views/home.php
+++ app/application/views/home.php
@@ -11,8 +11,8 @@
 		<div id="myCarousel" class="carousel slide" data-ride="carousel" style="width:100%; height:100%;">
   		<!-- Indicators -->
 		  <ol class="carousel-indicators">
-		    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-		    <li data-target="#myCarousel" data-slide-to="1"></li>
+		    <li data-target="#myCarousel" data-slide-to="0"></li>
+		    <li data-target="#myCarousel" data-slide-to="1" class="active"></li>
 		    <li data-target="#myCarousel" data-slide-to="2"></li>
 		    <li data-target="#myCarousel" data-slide-to="3"></li>
 		    <li data-target="#myCarousel" data-slide-to="4"></li>
@@ -20,23 +20,41 @@
 		  </ol>
 		  <?php include_once($this->config->item('base_path') . 'application/views/_header_top_bar.php'); ?>
 	        <div class="carousel-inner" style="width:100%; height:100%;">
-	            <div id='page1' class="item active">
-	            	<p>hello page 1</p>
+	            <div id='page1' class="item">
 	            </div>
-	            <div id='page2' class="item">
-	            	<p>hello page 2</p>
+	            <div id='page2' class="item active">
+<!-- 	            	<div class="profile-image">
+					  <a href="#"><img src="/assets/img/page2/creative_icon.png" /></a>
+					  <span class="overlay"> <img src="/assets/img/page2/creative_icon_overlay.png" /> </span>
+					</div> -->
+					<div class="top top-strategy">
+						<div class="first"><img src="/assets/img/page2/strategy_icon.png" /></div>
+					    <div class="second"><img src="/assets/img/page2/strategy_icon_overlay.png" alt="Overlay Image" /></div>
+					</div>
+
+					<div class="top top-creative">
+						<div class="first"><img src="/assets/img/page2/creative_icon.png" /></div>
+					    <div class="second"><img src="/assets/img/page2/creative_icon_overlay.png" alt="Overlay Image" /></div>
+					</div>
+
+					<div class="top top-development">
+						<div class="first"><img src="/assets/img/page2/development_icon.png" /></div>
+					    <div class="second"><img src="/assets/img/page2/development_icon_overlay.png" alt="Overlay Image" /></div>
+					</div>
+
+					<div class="top top-experimential">
+						<div class="first"><img src="/assets/img/page2/experimential_icon.png" /></div>
+					    <div class="second"><img src="/assets/img/page2/experimential_icon_overlay.png" alt="Overlay Image" /></div>
+					</div>
+
 	            </div>
 	            <div id='page3' class="item">
-	            	<p>hello page 3</p>
 	            </div>
 	            <div id='page4' class="item">
-	            	<p>hello page 4</p>
 	            </div>
 	            <div id='page5' class="item">
-	            	<p>hello page 5</p>
 	            </div>
 	            <div id='page6' class="item">
-	            	<p>hello page 6</p>
 	            </div>
 	        </div>
 	        <a class="left carousel-control" href="#myCarousel" data-slide="prev"><img src="/assets/img/arrow_icon_left.png" id="arrow_icon"></a>
